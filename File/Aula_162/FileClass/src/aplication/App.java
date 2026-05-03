ef2d3728f8d43494433b124080336f85c15fe0ee
Barbara Geovana <barbara.geovanans@gmail.com>
2026-05-03 16:41:50 -0300

    Aula 163: FileReader e BufferedReader

--- File/Aula_162/FileClass/src/aplication/App.java
+++ File/Aula_162/FileClass/src/aplication/App.java
@@ -1,25 +1,37 @@
 package aplication;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\temp.txt");
-        Scanner sc = null;
-        
+        String path = "C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\temp.txt";
+        FileReader file = null;
+        BufferedReader buffer = null;
+
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            file = new FileReader(path);
+            buffer = new BufferedReader(file);
+
+            String line = buffer.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = buffer.readLine();
             }
         } catch (IOException e) {
-            System.out.println("Erro " + e.getMessage());
-        }finally{
-           if(sc != null){sc.close();}
-        }
-
-    }
-
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (buffer != null) {
+                    buffer.close();
+                }
+                if (file != null) {
+                    file.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error ao fechar o arquivo: " + e.getMessage());
+            }
+        }  
+    }   
 }
