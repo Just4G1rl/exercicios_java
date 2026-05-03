d693f8bc6b5f4dc6566b472656360869df7fab23
Barbara Geovana <barbara.geovanans@gmail.com>
2026-05-03 16:52:52 -0300

    Aula 164: try with resourses

--- File/Aula_162/FileClass/src/aplication/App.java
+++ File/Aula_162/FileClass/src/aplication/App.java
@@ -7,13 +7,7 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) {
         String path = "C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\temp.txt";
-        FileReader file = null;
-        BufferedReader buffer = null;
-
-        try {
-            file = new FileReader(path);
-            buffer = new BufferedReader(file);
-
+        try (BufferedReader buffer = new BufferedReader(new FileReader(path))){
             String line = buffer.readLine();
             while (line != null) {
                 System.out.println(line);
@@ -21,17 +15,6 @@ public class App {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (buffer != null) {
-                    buffer.close();
-                }
-                if (file != null) {
-                    file.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Error ao fechar o arquivo: " + e.getMessage());
-            }
-        }  
+        }
     }   
 }
