3d0d320962c853af2c2628892ec8ff22de25c8b1
Barbara Geovana <barbara.geovanans@gmail.com>
2026-05-03 19:43:13 -0300

    Aula 165: BufferedWriter e FileWriter

--- File/Aula_162/FileClass/src/aplication/App.java
+++ File/Aula_162/FileClass/src/aplication/App.java
@@ -1,17 +1,18 @@
 package aplication;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\temp.txt";
-        try (BufferedReader buffer = new BufferedReader(new FileReader(path))){
-            String line = buffer.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = buffer.readLine();
+
+        String[] lines = new String[] {"Hola", "Oi", "Hello","Bonjour", "Ciao"};
+        String path = "C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\out.txt";
+        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(path))){
+            for(String line : lines) {
+                bWriter.write(line);
+                bWriter.newLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
