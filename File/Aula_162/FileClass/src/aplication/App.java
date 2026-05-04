bef8e9381708d5fd068b23a93fccc6610d4aa6b6
Barbara Geovana <barbara.geovanans@gmail.com>
2026-05-04 16:51:28 -0300

    Aula: 166: manipulação de pastas com File

--- File/Aula_162/FileClass/src/aplication/App.java
+++ File/Aula_162/FileClass/src/aplication/App.java
@@ -1,21 +1,28 @@
 package aplication;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
-        String[] lines = new String[] {"Hola", "Oi", "Hello","Bonjour", "Ciao"};
-        String path = "C:\\Users\\Barbara\\OneDrive\\Área de Trabalho\\Barbara\\out.txt";
-        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(path))){
-            for(String line : lines) {
-                bWriter.write(line);
-                bWriter.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        System.out.println("Enter a file path: ");
+        String strPath = scn.nextLine();
+        
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+
+        boolean success = new File(strPath + "\\Teste").mkdir();
+        if (success) {
+            System.out.println("Directory created successfully!");
+        } else {
+            System.out.println("Failed to create directory.");
         }
     }   
 }
